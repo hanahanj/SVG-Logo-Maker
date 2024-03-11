bfe2f433a70acc42f1afe9764a9ff86208f2bfae
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-03-11 10:11:53 -0500

    tweaked design of square and triangle logos, added some to examples, built out circle test but still failing

--- shapes.js
+++ shapes.js
@@ -12,10 +12,8 @@ return `
 <svg version="1.1"
      width="300" height="200"
      xmlns="http://www.w3.org/2000/svg">
-
-  <circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
-
+     <circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />
+     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
 </svg>
 `
   };
@@ -35,7 +33,7 @@ return `
      xmlns="http://www.w3.org/2000/svg">
 
      <rect width="100%" height="100%" fill="${this.shapeColor}" />
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+     <text x="150" y="180" font-size="100" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
 
 </svg>
 `
@@ -55,7 +53,7 @@ return `
      width="300" height="300"
      xmlns="http://www.w3.org/2000/svg">
      <polygon points="150, 18 244, 182 56, 182" fill="${this.shapeColor}" />
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+     <text x="150" y="130" font-size="40" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
 
 </svg>
 `
--- shapes.test.js
+++ shapes.test.js
@@ -4,20 +4,18 @@ const {Circle, Square, Triangle} = require('./shapes.js');
 // const shape = new Triangle();
 // expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
 
-describe('Password Length', () => {
-    it('should return false for password less than 8 characters long', () => {
-        const shape = new Circle('`1', '`1', '`1' );
-    
-        expect(shape.render()).toEqual(`
+describe('Circle Test', () => {
+    it('should return true if a circular logo with the correct color, text and text color is not generated', () => {
+        const shape = new Circle('ABC', 'White', 'Black' );
+        const expectedCircle = `
         <svg version="1.1"
              width="300" height="200"
              xmlns="http://www.w3.org/2000/svg">
-        
-          <circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />
-        
-          <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
-        
+             <circle cx="150" cy="100" r="80" fill="Black" />
+             <text x="150" y="125" font-size="60" text-anchor="middle" fill="White">ABC</text>
         </svg>
-        `);
+        `
+        expect(shape.render()).toEqual(expectedCircle);
+
     });
   });
